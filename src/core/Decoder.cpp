d01b9726c87a1c65e6a8c3e565f13d0f3c87193c
GieniuG <bartoszlwowski@gmail.com>
2026-06-05 14:44:30 +0200

    add more opcodes

--- src/core/Decoder.cpp
+++ src/core/Decoder.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <stdexcept>
 
-
 void Decoder::DecodeAndExecute(CPU &cpu, uint16_t opcode) {
   std::cout << "opcode: " << std::hex << opcode << std::endl;
   uint16_t nnn = opcode & 0x0FFF;
@@ -14,6 +13,8 @@ void Decoder::DecodeAndExecute(CPU &cpu, uint16_t opcode) {
   uint8_t x = (opcode & 0x0F00) >> 8;
   uint8_t y = (opcode & 0x00F0) >> 4;
 
+  auto vx=cpu.registers[x],
+       vy=cpu.registers[y];
   switch (opcode & 0xF000) {
   case 0x0000:
     switch (nn) {
@@ -21,7 +22,10 @@ void Decoder::DecodeAndExecute(CPU &cpu, uint16_t opcode) {
       cpu.display->Reset();
       break;
     case 0xEE:
-      std::cout << "NOT IMPLEMENTED 0xEE" << std::endl;
+      cpu.PC=cpu.stack[cpu.SP-1];
+      cpu.stack[cpu.SP]=0;
+      if(cpu.SP>0)
+          cpu.SP--;
       break;
     default:
       std::runtime_error("Opcode not recognized");
@@ -30,14 +34,86 @@ void Decoder::DecodeAndExecute(CPU &cpu, uint16_t opcode) {
   case 0x1000:
     cpu.PC=nnn;
     break;
+  case 0x2000:
+    cpu.stack[cpu.SP]=cpu.PC;
+    if(cpu.SP<16)
+        cpu.SP++;
+    cpu.PC=nnn;
+    break;
+  case 0x3000:
+    if(vx==nn)
+        cpu.PC+=2;
+    break;
+  case 0x4000:
+    if(vx!=nn)
+        cpu.PC+=2;
+    break;
+  case 0x5000:
+    if(vx==vy)
+        cpu.PC+=2;
+    break;
   case 0x6000:
     cpu.registers[x] = nn;
     break;
   case 0x7000:
     cpu.registers[x]+=nn;
     break;
+  case 0x8000:
+    switch(n){
+      case 0x0:
+        cpu.registers[x]=vy;
+        break;
+      case 0x1:
+        cpu.registers[x]=vx|vy;
+        break;
+      case 0x2:
+        cpu.registers[x]=vx&vy;
+        break;
+      case 0x3:
+        cpu.registers[x]=vx^vy;
+        break;
+      case 0x4:
+        {
+          uint16_t sum = vx+vy;
+          cpu.registers[x]=sum &0xFF;
+          cpu.registers[0xf]=sum>255?1:0;
+        }
+        break;
+      case 0x5:
+        {
+          uint16_t diff = vx-vy;
+          cpu.registers[x]=diff &0xFF;
+          cpu.registers[0xf]=diff>255?0:1;
+        }
+        break;
+      case 0x6:
+        cpu.registers[0xf]=vx&0x001;
+        cpu.registers[x]=vx>>1;
+        break;
+      case 0x7:
+        {
+          uint16_t diff = vy-vx;
+          cpu.registers[x]=diff &0xFF;
+          cpu.registers[0xf]=diff>255?0:1;
+        }
+        break;
+      case 0xe:
+        cpu.registers[x]=vx<<1;
+        break;
+    }
+    break;
+  case 0x9000:
+    if(vx!=vy)
+        cpu.PC+=2;
+    break;
   case 0xA000:
     cpu.I = nnn;
+    break;
+  case 0xB000:
+    cpu.PC=nnn+cpu.registers[0];
+    break;
+  case 0xC000:
+    cpu.registers[x]=(rand()%255)&nn;
     break;
   case 0xD000:
     cpu.registers[0xF]=0;
@@ -46,9 +122,9 @@ void Decoder::DecodeAndExecute(CPU &cpu, uint16_t opcode) {
         uint8_t mask = 0b10000000;
         for(int column=0;column<8;column++){
           uint8_t bit = (line & mask) >> (7-column);
-          uint8_t cx=(cpu.registers[x]%64)+column,
-                  cy=(cpu.registers[y]%32)+row;
-          if(cx>64 || cy > 32){
+          uint8_t cx=(vx%64)+column,
+                  cy=(vy%32)+row;
+          if(cx>=64 || cy >= 32){
               continue;
           }
           uint8_t currPixel = cpu.display->GetPixel(cx,cy);
