c45f84a9fadf1bdc68a999e88d629c338026f848
GieniuG <bartoszlwowski@gmail.com>
2026-06-05 14:44:53 +0200

    add keypad

--- src/gui/App.cpp
+++ src/gui/App.cpp
@@ -6,8 +6,10 @@
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Window/VideoMode.hpp>
+#include <cstdint>
 #include <iostream>
 #include <stdexcept>
+#include <unordered_map>
 
 App::App()
     : window(sf::VideoMode({640, 320}), "chip8"), sprite(texture), emulator(),
@@ -33,29 +35,34 @@ App::App()
 }
 
 void App::Run() {
-  emulator.LoadROM("roms/IBM Logo.ch8");
+  std::unordered_map<sf::Keyboard::Scancode, uint8_t> keyMap={
+      {sf::Keyboard::Scan::Num1,0x1},{sf::Keyboard::Scan::Num2,0x2},{sf::Keyboard::Scan::Num3,0x3},{sf::Keyboard::Scan::Num4,0xc},
+      {sf::Keyboard::Scan::Q,0x4},{sf::Keyboard::Scan::W,0x5},{sf::Keyboard::Scan::E,0x6},{sf::Keyboard::Scan::R,0xd},
+      {sf::Keyboard::Scan::A,0x7},{sf::Keyboard::Scan::S,0x8},{sf::Keyboard::Scan::D,0x9},{sf::Keyboard::Scan::F,0xe},
+      {sf::Keyboard::Scan::Z,0xa},{sf::Keyboard::Scan::X,0x0},{sf::Keyboard::Scan::C,0xb},{sf::Keyboard::Scan::V,0xf},
+  };
+  emulator.LoadROM("../roms/3-corax+.ch8");
+  Keypad keypad=emulator.GetKeypad();
   window.setFramerateLimit(60);
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
         window.close();
+      if(event.type == sf::Event::KeyPressed || event.type == sf::Event::KeyReleased){
+          if(keyMap.count(event.key.scancode)){
+              uint8_t key=keyMap[event.key.scancode];
+              uint8_t state = event.type == sf::Event::KeyPressed;
+
+              if(state != keypad.IsKeyPressed(key)){
+                  keypad.SetKey(key,state);
+              }
+          }
+      }
     }
     emulator.Tick();
     window.clear();
     auto buff = emulator.GetDisplay().GetBuffer();
-            for(int i=0;i<64*32;i++){
-            if(i%64==0){
-                std::cout<<std::endl;
-            }
-            if(buff[i]){
-                std::cout<<"█";
-            }else{
-                std::cout<<" ";
-            }
-        }
-        std::cout<<std::endl;
-
     for (int i = 0; i < 2048; i++) {
       if (buff[i]) {
         for (int j = 0; j < 4; j++) {
